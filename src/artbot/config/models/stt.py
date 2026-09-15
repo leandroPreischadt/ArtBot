9d21793add42425f74db9fd97608f584ea952e8b
leandro <leandroep10@gmail.com>
2026-09-15 14:19:03 -0300

    textos auxiliares organizados

--- src/artbot/config/models/stt.py
+++ src/artbot/config/models/stt.py
@@ -7,30 +7,31 @@ import numpy as np
 
 def load_model():
     
-    """variáveis do modelo"""
+    """Models vars"""
     model_size = "small"
     device_type = "cpu"
     compute_type = "int8"
+    
+    """initiating model"""
     model = WhisperModel(model_size, device=device_type, compute_type=compute_type)
     
     return model
 
 def whisper_model(model): 
     
-    """variáveis para gravar o audio"""
+    """Recording Vars"""
     duration = 7.0
     fs = 16000
     number_channels = 1
     
-    print("gravando...")
+    print("Recording...")
     myrecording = sd.rec(int(duration * fs), samplerate=fs, channels=number_channels)
     sd.wait()
-    print("Gravação finalizada")
+    print("Recording finished")
     
     myrecording = myrecording.squeeze()
     myrecording = np.ascontiguousarray(myrecording, dtype=np.float32)
 
-    """inicialização do modelo"""
 
     segments, info = model.transcribe(myrecording, beam_size=5, vad_filter=False)
             
