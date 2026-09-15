d0acce36ca3f1700717ec7a64e91e41b03be183e
leandro <leandroep10@gmail.com>
2026-09-15 13:57:03 -0300

    STT finalizado

--- src/artbot/__init__.py
+++ src/artbot/__init__.py
@@ -1,6 +1,7 @@
 from artbot.config.paths import MODELS_DIR
 from artbot.config.settings import DEVICE, MODEL_NAME
 from artbot.config.models.stt import whisper_model
+from artbot.config.models.stt import *
 
 MODEL_PATH = MODELS_DIR / MODEL_NAME
 
@@ -8,8 +9,12 @@ MODEL_PATH = MODELS_DIR / MODEL_NAME
 def main() -> None:
     print(f"MODEL: {MODEL_PATH}")
     print(f"DEVICE: {DEVICE}")
-    whisper_model()
     
+    loaded_model = load_model()
+    
+    while True:
+        input("Press enter to start recording: ")
+        my_text = whisper_model(loaded_model)
 
 
 if __name__ == "__main__":
--- src/artbot/config/models/stt.py
+++ src/artbot/config/models/stt.py
@@ -1,14 +1,43 @@
 from faster_whisper import WhisperModel
 from artbot.config.settings import DEVICE
+import io
+import sounddevice as sd
+import numpy as np
 
-def whisper_model():
-    # Initialize model (use device="cuda" if you have a compatible GPU)
-    model = WhisperModel("small", device=DEVICE, compute_type="int8")
 
-    # Transcribe audio file
-    segments, info = model.transcribe("audio.mp3", beam_size=5)
+def load_model():
+    
+    """variáveis do modelo"""
+    model_size = "small"
+    device_type = "cpu"
+    compute_type = "int8"
+    model = WhisperModel(model_size, device=device_type, compute_type=compute_type)
+    
+    return model
 
-    print(f"Detected language '{info.language}' with probability {info.language_probability:.2f}")
+def whisper_model(model): 
+    
+    """variáveis para gravar o audio"""
+    duration = 7.0
+    fs = 16000
+    number_channels = 1
+    
+    print("gravando...")
+    myrecording = sd.rec(int(duration * fs), samplerate=fs, channels=number_channels)
+    sd.wait()
+    print("Gravação finalizada")
+    
+    myrecording = myrecording.squeeze()
+    myrecording = np.ascontiguousarray(myrecording, dtype=np.float32)
 
+    """inicialização do modelo"""
+
+    segments, info = model.transcribe(myrecording, beam_size=5, vad_filter=False)
+            
+    text = ""
+    
     for segment in segments:
-        print(f"[{segment.start:.2f}s -> {segment.end:.2f}s] {segment.text}")
+        text += segment.text
+    print(text)
+    
+    return text 
