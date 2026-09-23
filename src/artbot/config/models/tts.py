51bae42a4347794c6b5173f488400af0dd4553bd
leandro <leandroep10@gmail.com>
2026-09-23 13:52:32 -0300

    Iniciando banco de memórias para a llm

--- src/artbot/config/models/tts.py
+++ src/artbot/config/models/tts.py
@@ -1,10 +1,8 @@
 from piper import PiperVoice
 import sounddevice as sd
-import numpy as np
 from artbot.config.settings import TTS_MODEL_NAME
 
 # Piper + processamento de áudio -> tentar fazer para uma voz mais robótica 
-
 
 def load_piper_model():
     voice = PiperVoice.load(TTS_MODEL_NAME)
@@ -14,8 +12,6 @@ def piper_model(voice, text):
     with sd.RawOutputStream(
     samplerate=voice.config.sample_rate,
     channels=1,
-    dtype="int16",
-) as stream:
+    dtype="int16",) as stream:
         for chunk in voice.synthesize(text):
             stream.write(chunk.audio_int16_bytes)
-    
